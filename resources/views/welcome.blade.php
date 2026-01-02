4289eea84a9f33a260e0ac359928caf066753589
WesleyRomain <146979701+WesleyRomain@users.noreply.github.com>
2026-01-02 12:38:57 +0100

    Profielen klikbaar maken voor bezoekers (redirect naar de profielpagina van de betrokken gebruikers).

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,15 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>Welkom bij Yesterdayland</h1>
+    <h1>Welkom bij Yesterdayland!</h1>
 
     <h2>Alle bewoners</h2>
     <div class="profile-grid">
         @foreach($users as $user)
-            <div class="profile-item">
-                <img src="{{ asset ('storage/' . $user->profile_picture) }}" alt="Foto van {{ $user->name }}">
-                <p> {{ $user->name }}</p>
-            </div>
+            <a href=" {{ route('profile.show', $user) }}" class="profile-item"> {{--Aangepast zodat mensen kunnen doorklikken naar de profielpagina'ss van de users--}}
+                <img src="{{ asset('storage/' . $user->profile_picture) }}" alt="Foto van {{ $user->name }}">
+                <p>{{ $user->name }}</p>
+            </a>
         @endforeach
     </div>
 
