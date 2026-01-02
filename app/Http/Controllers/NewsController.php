7c71015699d00e1fac9edef2641826acf817ba00
WesleyRomain <146979701+WesleyRomain@users.noreply.github.com>
2026-01-02 21:18:28 +0100

    Controller vormgegeven: functies aangepast naar noden. Morgen views genereren en admin-layout maken.

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -19,23 +19,31 @@ class NewsController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() // Als functie wordt opgeroepen, stuur door naar admin.news.create
     {
-        //
+        return view('admin.news.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request) // Een nieuw nieuwsitem opslaan: eerst valideren of alle gegevens aanwezig zijn
     {
-        //
+        $validated= $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'published_at' => 'required|date',
+        ]);
+
+        News::create($validated); // Een nieuwe instantie van News aanmaken.
+
+        return redirect()->route('news.index'); // Stuur terug door naar de beginpagina.
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(News $news)
+    public function show(News $news) // Tonen van specifieke detailpagina.
     {
         return view('news.show', compact('news'));
     }
@@ -43,24 +51,35 @@ class NewsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(News $news) // Bij aanroepen functie -> stuur door naar view: admin.news.edit .
     {
-        //
+        return view('admin.news.edit', compact('news'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, News $news) // Bij aanroepen functie: valideer gegevens en update het newsitem als alles oké is.
     {
-        //
+        $validated= $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'published_at' => 'required|date',
+        ]);
+
+        $news->update($validated);
+
+        return redirect()->route('news.index');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(News $news) // Verwijderen van item.
     {
-        //
+        $news->delete();
+
+        return redirect()->route('admin.news.index');
     }
 }
