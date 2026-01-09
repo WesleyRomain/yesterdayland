168ea97c17c46f9c544309384704098ab20012ab
WesleyRomain <146979701+WesleyRomain@users.noreply.github.com>
2026-01-09 13:32:12 +0100

    Contactformulier: foutje in comments

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -3,6 +3,6 @@
 <p><strong>Bericht:</strong></p> {{--Toont het bericht--}}
 <p>{{ $data['message'] }}</p>
 
-{{----Kijken in storage-> log onderaan om te kijken of een mail gestuurd wordt. Admin is immers een fictieve beheerder (geen echte mail gestuurd) + .env aanpassen}}
+{{--Kijken in storage-> log onderaan om te kijken of een mail gestuurd wordt. Admin is immers een fictieve beheerder (geen echte mail gestuurd) + .env aanpassen--}}
 
 
